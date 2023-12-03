ff7281d8d9e52b8d89d5198eca0bd7371a2c6cec
AbhinandNtk <abhinadntkm@gamail.com>
2023-12-03 18:10:12 +0530

    Modified

--- djangoproject/project/project/settings.py
+++ djangoproject/project/project/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 'your_app_name.custom_context.custom_context',
             ],
         },
     },
